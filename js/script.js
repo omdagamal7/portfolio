2424b74acfcb283a2bfa301594713ff9fe372b35
OMDV <trfly18@gmail.com>
2023-10-12 18:23:21 +0300

    all is done

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 const btn = document.getElementById('cv')
-const url = 'https://download2277.mediafire.com/2c0r4ah26fug3W_5DrPASqKzoHZG7S0UuIgB9MPL3_D8HqZ8sQIejgOHsYO4Wh0u1vx6USffGYQv0OKu-avqpi8wh7UYpTFL1Auhz5Smx9dkd1Bg5iEiCrXJuhu5sTjpCIEN78h5HufaShsD2XUGXZ8l6IQwGHyjQoEeYAx53Bg53g/dcywiyf9cca2nj6/Emad+Jammal+Resume.pdf'
+const path = '../files/Emad Jammal Resume.pdf'
 btn.addEventListener('click', function (e) {
   e.preventDefault()
   const anchor = document.createElement('a')
-  anchor.href = url;
+  anchor.href = path;
+  anchor.download
   anchor.click()
-  anchor.remove()
 
 })
 
